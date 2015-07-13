26e06d00c73a24b199ec1700a6ed3bc22ec692a9
Zortax <leonard.seibold.m@gmail.com>
2015-07-13 20:29:35 +0200

    Stats

--- src/de/zortax/oneshot/game/GameManager.java
+++ src/de/zortax/oneshot/game/GameManager.java
@@ -6,8 +6,20 @@ public class GameManager {
 
 	private OneShot os;
 	
+	private GameState state;
+	
 	public GameManager(OneShot os){
 		this.os = os;
+		state = GameState.LOBBY;
+	}
+	
+	
+	
+	
+	
+	//Getter-Methoden
+	public GameState getState(){
+		return state;
 	}
 	
 }
